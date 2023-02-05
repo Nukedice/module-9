74c0c5e1aecec7127a6c5fc2ed26161d154797bd
Nukedice <120702168+Nukedice@users.noreply.github.com>
2023-02-05 16:25:32 +0500

    Add files via upload

---
+++ task 4/4.js
@@ -0,0 +1,16 @@
+const btn = document.querySelector('.btn');
+btn.addEventListener('click', () => {
+  const value1 = +document.getElementById('input1').value;
+  const value2 = +document.getElementById('input2').value;
+  let result = document.getElementById('j-result');
+  result.textContent = '';
+  if (!(value1 >= 100 && value1 <= 300 && value2 >= 100 && value2 <= 300)) {
+    result.textContent = 'одно из чисел вне диапазона от 100 до 300';
+    return;
+  }
+  fetch(`https://picsum.photos/${value1}/${value2}`)
+    .then((response) => {
+      document.getElementById('result').src = response.url;
+    });
+
+});
